20fcf3c1a9c7c12e048937a3262cf1238e4ddb8b
Laura Belén Yachelini <lbyachelini@gmail.com>
2024-07-05 07:25:13 -0300

    first commit

---
+++ tarea1/script.js
@@ -0,0 +1,54 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+    const buttonContraste = document.getElementById('contraste')
+    const formulario = document.getElementById('formulario');
+    const botonEnviar = document.getElementById('enviar');
+    let modoContraste = false;
+    let formularioCompleto = false;
+
+    formulario.addEventListener('submit', (event) => {
+        event.preventDefault(); 
+    });
+
+    botonEnviar.addEventListener('click', () => {
+        const nombre = document.getElementById('nombre').value;
+        const apellido = document.getElementById('apellido');
+        const nacimiento = document.getElementById('nacimiento').value;
+        const mail = document.getElementById("mail").value;
+        const constraste = document.getElementById('contraste').value;
+        validarFormulario(nombre, apellido, nacimiento, mail);
+        });
+
+    buttonContraste.addEventListener('click', () => {
+     
+        if (modoContraste){
+            formulario.className = 'original';
+            modoContraste = false;
+            buttonContraste.textContent = 'Cambiar a original';
+
+        } else {
+            buttonContraste.textContent = 'Cambiar a contraste';
+            formulario.className = 'contraste';
+            modoContraste = true;
+        }
+    });
+
+});
+
+function emptyAlert(input) {
+    if (input === '') {  
+        alert(`Por favor ingrese un valor`);
+       return true;
+    }
+}  
+
+function validarFormulario(nombre, apellido, nacimiento, mail){
+    if(!emptyAlert(nombre) &&
+    !emptyAlert(apellido) &&
+    !emptyAlert(nacimiento) &&
+    !emptyAlert(mail)
+){
+    alert(`Formulario enviado con exito`);
+
+}}
+
+
