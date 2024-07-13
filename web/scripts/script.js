b0d210f902f5d3c94db7146e5a5649ff193b9a05
Laura Belén Yachelini <lbyachelini@gmail.com>
2024-07-13 09:19:04 -0300

    Agregando bootstrap al proyecto

--- web/scripts/script.js
+++ web/scripts/script.js
@@ -2,7 +2,10 @@ document.addEventListener('DOMContentLoaded', ()=> {
     const body = document.body;
     const buttonContraste = document.getElementById('contraste');
     const formulario = document.getElementById('formulario');
-    const botonEnviar = document.getElementById('enviar');
+    const botonEnviar = document.getElementById('form-submit');
+    const card = document.getElementById('form-card');
+    const nav = document.getElementById('nav');
+    const footer = document.getElementById('footer');
     let modoContraste = false;
     let formularioCompleto = false;
 
@@ -13,24 +16,28 @@ document.addEventListener('DOMContentLoaded', ()=> {
     botonEnviar.addEventListener('click', () => {
         const nombre = document.getElementById('nombre').value;
         const apellido = document.getElementById('apellido').value;
-        const nacimiento = document.getElementById('nacimiento').value;
         const mail = document.getElementById("mail").value;
         const constraste = document.getElementById('contraste').value;
-        validarFormulario(nombre, apellido, nacimiento, mail);
+        validarFormulario(nombre, apellido, mail);
         });
 
     buttonContraste.addEventListener('click', () => {
      
         if (buttonContraste.textContent === 'Cambiar a original'){
-            formulario.className = 'original';
+         
             body.className  = 'original';
-            //modoContraste = false;
+            card.className = 'card text-bg-light mx-auto';
+            nav.className = 'navbar navbar-expand-lg bg-body-tertiary'; 
+            footer.className = 'bg-body-tertiary text-center';
+
             buttonContraste.textContent = 'Cambiar a contraste';
 
         } else {
             buttonContraste.textContent = 'Cambiar a original';
-            formulario.className = 'contraste';
             body.className  = 'contraste';
+            card.className = 'card text-bg-dark mx-auto';
+            nav.className = 'navbar navbar-expand-lg bg-dark'
+            footer.className = 'text-bg-dark text-center'
             modoContraste = true;
         }
     });
@@ -39,7 +46,7 @@ document.addEventListener('DOMContentLoaded', ()=> {
 
 function emptyAlert(valor) {
     if (valor == null || valor.length == 0 || /^\s+$/.test(valor)) {  
-        alert(`Por favor ingrese un valor valido`);
+        alert(`Hay campos sin completar. Por favor rellene los datos requeridos.`);
        return false;
     }else{
         return true;
@@ -49,10 +56,9 @@ function emptyAlert(valor) {
 }  
 
 
-function validarFormulario(nombre, apellido, nacimiento, mail){
+function validarFormulario(nombre, apellido, mail){
     if(emptyAlert(nombre) &&
     emptyAlert(apellido) &&
-    emptyAlert(nacimiento) &&
     emptyAlert(mail)
 ){
     alert(`Formulario enviado con exito`);
